3f94fc8fda6c06674af29f1d4547ed01577c3c7d
swaroop panigrahi <swaroop.sp@gmail.com>
2020-03-03 10:36:41 +0000

    first commit

---
+++ src/_services/user.service.js
@@ -0,0 +1,29 @@
+import config from 'config';
+import { authHeader } from '../_helpers';
+
+export const userService = {
+    login,
+    logout
+};
+
+function login(username, password) {
+
+    return fetch('http://localhost:3000/users/authenticate',
+    {
+        method: 'post',
+        crossDomain:true,
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then(responseJson => {
+        console.log(" --------------" + responseJson);
+        // user.authdata = window.btoa(username + ':' + password);
+        localStorage.setItem('user', JSON.stringify(responseJson));
+    });
+}
+
+function logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('user');
+}
